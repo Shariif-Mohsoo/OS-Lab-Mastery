5dc6c64decce2451ffcde8cef85ca7d7cc9e9439
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-26 21:30:35 +0500

    Open URL in web

---
+++ batchScripting/01_OPEN_URL.bat
@@ -0,0 +1,6 @@
+@echo off
+Title OPEN_URL_01 BATCH SCRIPTING
+echo There is the only one key press and your tabs will be open in browser.
+pause
+start chrome.exe www.youtube.com
+start chrome.exe www.github.com/Shariif-Mohsoo
