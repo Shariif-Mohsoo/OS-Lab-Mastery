7d660dc93eca9c21748bf5cbfbb5f93ea0b782fc
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-01 03:15:07 -0800

    Script to get conditional input

--- batchScripting/07_YesNo_UserInput.bat
+++ batchScripting/07_YesNo_UserInput.bat
@@ -6,9 +6,11 @@ cls
 
 set /p user_input=Do you like to continue(Y/N)?:
 
-if not defined %user_input% goto start
+
+if not defined user_input goto start
 if /i %user_input%==Y goto Yes
 if /i %user_input%==N (goto No) else (goto invalid)
+
 
 :Yes
 echo User has entered yes.
