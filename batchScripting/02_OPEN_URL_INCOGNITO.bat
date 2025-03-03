1a97002ad00af3a659c6b52e2c24e639a1bc6df6
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-03 16:17:45 +0500

    Opening Tab's in incognito mode

---
+++ batchScripting/02_OPEN_URL_INCOGNITO.bat
@@ -0,0 +1,6 @@
+@echo off
+Title OPEN_URL_INCOGNIOTO_02 BATCH SCRIPT
+echo Dear just one key enter and your some tabs will be opened in browser.
+pause
+start chrome.exe -incognito www.google.com
+start msedge.exe -inprivate www.youtube.com
