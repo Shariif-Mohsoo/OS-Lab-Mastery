26332f954cdd0b508e18e5eaece13b166659d8a1
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-02 18:59:22 -0800

    Script to create the folder;

--- batchScripting/08_Create_Folder.bat
+++ batchScripting/08_Create_Folder.bat
@@ -3,5 +3,5 @@ Title Create New Folder
 set /p folder_name=Enter the new folder name:
 set /p folder_path=Enter the folder path:
 set new_path=%folder_path%\%folder_name%
-echo %new_path%
+mkdir %new_path%
 pause
