a310897853add6d415dbfad633f92c12f845cc63
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-04 17:56:56 -0800

    Loop through files from the given path

--- batchScripting/09_Loop_Through_Files.bat
+++ batchScripting/09_Loop_Through_Files.bat
@@ -2,5 +2,8 @@
 Title Loop Through Files
 ::for %%variable in (list) do (code)
 ::for %%i in (1,2,3,4,5) do (echo %%i)
-
+set /p folder_path=Dear enter the folder path:
+cd %folder_path%
+for %%i in (*.*) do (echo %%i)
 pause
+exit
