dfc17af51e4fce2ca91630ab699ef5bf36e9ccd2
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-07 12:22:41 -0800

    Practicing if-else-if

--- batchScripting/03_Practice.bat
+++ batchScripting/03_Practice.bat
@@ -1,12 +1,12 @@
 @echo off
 Title Let's practice if-else-if statements.
+:start
 echo Enter an option
 echo.
 echo 1) Start game
 echo 2) About
 echo 3) Exit
 echo.
-:start
 set /p choice=Enter the option:
 if %choice%==1 (
 	echo Game Started
