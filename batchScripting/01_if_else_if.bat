558b3931abf9d64235f07293e031566b9a3945ea
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-11 07:47:29 +0500

    If-else-If Practice

---
+++ batchScripting/01_if_else_if.bat
@@ -0,0 +1,39 @@
+@echo off
+echo Here we will see the if-else-if statement
+set n1=2;
+set n2=3;
+set /a sum= n1+n2
+::echo %sum%
+echo Sum
+if %sum% EQU 5 (
+    echo Equal
+) else if %sum% NEQ 5 (
+    echo Not Equal
+) else if %sum% GEQ 5 (
+    echo Greater Equal
+) else if %sum% LEQ 5 (
+    echo Less Equal
+) else if %sum% GTR 5 (
+    echo Greater than
+) else if %sum% LSS 5 (
+    echo Less than
+) else (
+    echo Unknown number
+)
+
+echo Both Number's Comparison
+if %n1% EQU %n1% (
+    echo Both Equal
+) else (
+    echo Both Unknown number's
+)
+if %n1% NEQ %n1% (
+    echo Both Not Equal
+) else (
+    echo Both Unknown number's
+)
+if %n1% GEQ %n1% (echo Both Greater Equal) else (echo Both Unknown number's)
+if %n1%  LEQ %n1% (echo Both Less Equal) else (echo Both Unknown number's)
+if %n1% GTR %n1% (echo Both Greater than) else (echo Both Unknown number's)
+if %n1% LSS %n1% (echo Both Less than) else (echo Both Unknown number's)
+pause
