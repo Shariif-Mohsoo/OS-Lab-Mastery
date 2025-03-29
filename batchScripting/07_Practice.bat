b3b3f262df9837754e08c08036559e8c1523cd93
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-29 12:39:57 +0500

    Three digit input caculator

--- batchScripting/07_Practice.bat
+++ batchScripting/07_Practice.bat
@@ -1,8 +1,17 @@
 @echo off
 Title Just practice dear.
-set person=Boy
-for /L %%i in (1,1,10) do (
-	echo Hello Boy %%i
-)
+set /p n1= Enter first number:
+set /p n2= Enter second number:
+set /p n3= Enter third number:
+echo ===========================
+echo Enter 1 for add           
+echo Enter 2 for sub           
+echo Enter 3 for mul	       
+echo Enter 4 for div           
+echo Enter 5 for mod	       
+echo ===========================
+echo.
+set /p choice= Enter your choice:
+echo %choice%
 
 pause
