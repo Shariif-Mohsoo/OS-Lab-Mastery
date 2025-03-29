76c1967deba2426a24e48892b6c2f8deab78a808
Shariif-Mohsoo <mr8781256@gmail.com>
2025-03-29 12:50:05 +0500

    Calculator

--- batchScripting/07_Practice.bat
+++ batchScripting/07_Practice.bat
@@ -1,8 +1,5 @@
 @echo off
 Title Just practice dear.
-set /p n1= Enter first number:
-set /p n2= Enter second number:
-set /p n3= Enter third number:
 echo ===========================
 echo Enter 1 for add           
 echo Enter 2 for sub           
@@ -12,6 +9,27 @@ echo Enter 5 for mod
 echo ===========================
 echo.
 set /p choice= Enter your choice:
-echo %choice%
-
+rem echo %choice%
+if %choice% GEQ 1 if %choice% LEQ 3 (
+set /p n1= Enter first number:
+set /p n2= Enter second number:
+set /p n3= Enter third number:
+if %choice% == 1 (
+	set /a res= n1+n2+n3
+) else if %choice% == 2 (
+	set /a res= n1-n2-n2
+) else if %choice% == 3 (
+	set /a res= n1*n2*n3
+)
+) else if %choice% GEQ 4 else if %choice% LEQ 5 (
+	
+set /p n1= Enter first number:
+set /p n2= Enter second number:
+	if %choice% == 4 (
+		set /a res= n1/n2
+	) else if %choice% == 5 (
+		set /a res= n1%%n2
+	)
+)
+echo Result is %res%
 pause
