8b5c7176c47e0064011fa9509bd2d4a894cd4b75
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-01 11:35:24 +0500

    Scanerio Practice

--- batchScripting/11_Practice.bat
+++ batchScripting/11_Practice.bat
@@ -4,7 +4,12 @@ set /p n1= Enter first number:
 set /p n2= Enter second number:
 if %n1% EQU %n2% (
 	echo n1 is equal to n2
-) else if %n1% NEQ %n2% (
+) else if %n1% LSS %n2% (
+	echo n1 is less than n2
+) else if %n1% GTR %n2% (
+	echo n1 is greater than n2
+) 
+if %n1% NEQ %n2% (
 	echo n1 is not equal to n2
-)
+) 
 Pause
