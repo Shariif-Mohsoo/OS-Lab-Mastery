2a3b1c3666f06850c28b9d6eb674a46ba933d33a
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-02 11:38:56 +0500

    Loop (Pattern) Practice

--- batchScripting/12_Practice.bat
+++ batchScripting/12_Practice.bat
@@ -1,4 +1,14 @@
 @echo off
 Title Pattern
+set enabledelayedexpansion
+set n= 10
+
+for /L %%i in (1,1,n) do (
+	
+)
+
+
 
 pause
+REM git add 12_Practice.bat
+REM git commit -m "Loop (Pattern) Practice"
