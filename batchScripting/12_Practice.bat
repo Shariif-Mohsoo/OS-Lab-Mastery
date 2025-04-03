261c34286566412fde7ad3db45f94ad4ef1751a4
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-03 17:30:41 +0500

    Loop (Pattern) Practice

--- batchScripting/12_Practice.bat
+++ batchScripting/12_Practice.bat
@@ -1,10 +1,30 @@
 @echo off
 Title Pattern
-set enabledelayedexpansion
-set n= 10
+setlocal enabledelayedexpansion
+set /p n= Enter the number:  
 
-for /L %%i in (1,1,n) do (
-	
+for /L %%i in (1,1,%n%) do (
+	REM echo %%i
+	set "line="
+	REM set /a spc= !n! - %%i
+	REM for /L %%j in (1,1,!spc!) do (
+		REM set "line=!line! "	
+	REM )
+
+	set /a spc= %n%-%%i
+    	for /L %%j in (1,1,!spc!) do (
+		REM THREE SPACES
+        	set "line=!line!   "
+    	)	
+
+	REM for /L %%k in (1,1,%%i) do (
+		REM set "line=!line!A "
+	REM )
+	set /a count= 2 * %%i - 1
+	for /L %%k in (1,1,!count!) do (
+		set "line=!line! A "
+	)
+	echo !line!
 )
 
 
