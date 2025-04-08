05d8a54d4b56240b2084e5903bd3bf7c69f22c67
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-08 18:05:25 +0500

    Array Attributes

--- batchScripting/14_Practice.bat
+++ batchScripting/14_Practice.bat
@@ -4,4 +4,10 @@ set person[0].fn= Ali
 set person[0].ln= Ahmed
 set person[0].age= 22
 set person[0].gender= Male
+for /L %%i in (0,1,0) do (
+	call echo First Name: %%person[%%i].fn%% 
+	call echo Last Name: %%person[%%i].ln%%
+	call echo Age= %%person[%%i].age%%
+	call echo Gender= %%person[%%i].gender%%
+)
 pause
