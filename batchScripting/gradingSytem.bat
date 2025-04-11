9ee42123d15a1a20e9e6fffebf31c48d449dfea7
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-11 11:49:58 +0500

    Grading System

--- batchScripting/gradingSytem.bat
+++ batchScripting/gradingSytem.bat
@@ -1,4 +1,34 @@
 @echo off
 Title Student Grading System
+:menu
+cls
+echo ============================
+echo     Student Grading System
+echo ============================
+set /p name=Enter student name: 
+set /p marks=Enter marks (out of 100): 
 
-Pause
+if %marks% GEQ 90 (
+    set grade=A
+) else if %marks% GEQ 80 (
+    set grade=B
+) else if %marks% GEQ 70 (
+    set grade=C
+) else if %marks% GEQ 60 (
+    set grade=D
+) else if %marks% GEQ 50 (
+   set grade=E
+) else (
+    set grade=F
+)
+
+echo.
+echo Student Name: %name%
+echo Marks: %marks%
+echo Grade: %grade%
+echo.
+
+pause
+goto menu
+
+
