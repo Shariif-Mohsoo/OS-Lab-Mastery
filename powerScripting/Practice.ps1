f30d091e78dfefdf5f7ff7d594cb866dcfa8ba67
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-24 17:58:23 +0500

    Practice loop and if statement

--- powerScripting/Practice.ps1
+++ powerScripting/Practice.ps1
@@ -1,4 +1,16 @@
-﻿for ($i = 1; $i -le 50; $i++){
+﻿# Number Checker: Loop with If-Else Conditions
 
-
+for ($i = 1; $i -le 50; $i++) {
+    if ($i % 5 -eq 0 -and $i % 2 -eq 0) {
+        Write-Host "$i ➤ Even and Divisible by 5" -ForegroundColor Cyan
+    }
+    elseif ($i % 5 -eq 0) {
+        Write-Host "$i ➤ Divisible by 5" -ForegroundColor Yellow
+    }
+    elseif ($i % 2 -eq 0) {
+        Write-Host "$i ➤ Even" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$i ➤ Odd" -ForegroundColor Magenta
+    }
 }
