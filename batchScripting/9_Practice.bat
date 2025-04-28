484cb179db8fc20a8516b8d2ad42792dad3a1247
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-28 22:56:53 +0500

    Right angle triangle pattern

--- batchScripting/9_Practice.bat
+++ batchScripting/9_Practice.bat
@@ -1,4 +1,12 @@
 @echo off
 Title Revert Right Angle Triangle
+setlocal enabledelayedexpansion
+	for /L %%i in (1,1,5) do (
+		set "line="
+		for /L %%j in (%%i,1,5) do (
+			set "line=!line!*"
+		)
+		echo !line!
+	)
 
 Pause
