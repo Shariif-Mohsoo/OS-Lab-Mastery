f63d5421e17c0ddf761db1c4db0d2b35dd807451
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-13 22:32:10 +0500

    While Loop In Bash Scripting

--- bashScript/while_1.sh
+++ bashScript/while_1.sh
@@ -4,8 +4,10 @@
 # while [ condition ] do #code done
 i=1
 while [ $i -le 5 ]
+#while [ $i<=5 ] #will result in an error.
 do
 	echo "Hello World"
 	echo "Mohsoo"
-	((i++))
+	#((i++)) can be written as below 
+	let i++
 done
