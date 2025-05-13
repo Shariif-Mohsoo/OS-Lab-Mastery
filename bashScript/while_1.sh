1e4711a35d05e217a803fda2949585405ba59b13
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-13 22:25:28 +0500

    While Loop

--- bashScript/while_1.sh
+++ bashScript/while_1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-#practing the while loop
-
+#Practing the while loop
+# Syntax for the while loop
+# while [ condition ] do #code done
+i=1
+while [ $i -le 5 ]
+do
+	echo "Hello World"
+	echo "Mohsoo"
+	((i++))
+done
