ef747491f730135da74867afe7d9ae7d931e5907
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-14 22:20:10 +0500

    Check even and odd number's from the given range

--- bashScript/forLoop_2.sh
+++ bashScript/forLoop_2.sh
@@ -3,7 +3,17 @@ read -p "Enter first number: " rangeStart
 read -p "Enter second number: " rangeEnd
 if [ $rangeStart -le $rangeEnd ]
 then
-	echo "Let's begin"
+	for((;rangeStart<=rangeEnd;rangeStart++))
+	{
+		res=$((rangeStart%2))
+		if [ $res -eq 0 ]
+		then
+			echo "$rangeStart is an even number"
+		else
+			echo "$rangeStart is an odd number"
+		fi
+	}
+
 else
 	echo "Range must be true"
 fi
