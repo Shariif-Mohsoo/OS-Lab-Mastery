c904dc733d09f7473ba6eee43a929d10daee1099
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-21 22:30:36 +0500

    Sum of array elements

--- bashScript/arr2.sh
+++ bashScript/arr2.sh
@@ -4,4 +4,16 @@ arr[1]=2
 arr[2]=3
 
 #printing length of an array
-echo Length of an array is: ${#arr[@]}
+len=${#arr[@]}
+echo Length of an array is: $len
+
+#printing an array through loop
+sum=0
+for ((i=0;i<len;i++))
+do
+	echo $i index element: ${arr[i]}
+	sum=$((sum+${arr[i]}))
+done
+
+#printing the sum of an array
+echo Sum of an array: $sum
