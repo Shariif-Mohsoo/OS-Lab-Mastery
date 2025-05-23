69716b853020c5dfef8eb2d9eb76724ce2f5c13f
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-23 12:26:04 +0500

    Finding the minimum from an array

--- bashScript/arr4.sh
+++ bashScript/arr4.sh
@@ -3,3 +3,16 @@
 scores=(50 30 22 99 100 55)
 #priting the socres
 echo Scores: ${scores[@]}
+#finding the min from an array
+min=${scores[0]}
+#finding the length of an array
+len=${#scores[@]}
+for (( i=1;i<len;i++ ))
+do
+	val=${scores[i]}
+	if [ $val -le $min ]
+	then
+		min=$val
+	fi
+done
+echo Minimum Scores: $min
