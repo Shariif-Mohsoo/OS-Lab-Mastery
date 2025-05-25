c24815cad9519c03d016284b152371ea2f36420a
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-25 19:03:40 +0500

    Finding sum of an array

--- bashScript/arr5.sh
+++ bashScript/arr5.sh
@@ -4,3 +4,15 @@ scores=(60 64 26 30 90)
 #finding the length of an array
 len=${#scores[@]}
 #finding the sum of an array
+i=0
+sum=0
+while [ $i -lt $len ]
+do
+	# echo $i
+	val=${scores[i]}
+	sum=$((sum+val))
+	let i++
+done
+#printing the sum
+echo Sum: $sum
+
