cc6b65212a8c679e6981dd87311f5225b8ca9752
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-31 12:14:16 +0500

    Prompt user to add variable value using read

--- bashScript/fullSyllabusPractice.sh
+++ bashScript/fullSyllabusPractice.sh
@@ -5,4 +5,8 @@ echo Hello Mohsoo
 #program to store value in variable.
 name="Mohsin"
 echo "Name: $name"
+#program to prompt user to add variable value.
+echo "Enter Name: "
+read name
+echo "Name you enter: $name"
 
