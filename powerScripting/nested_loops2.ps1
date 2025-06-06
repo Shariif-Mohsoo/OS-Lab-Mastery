368b3209ed8737fdc261fa39a989387528b893d3
Shariif-Mohsoo <mr8781256@gmail.com>
2025-06-06 18:13:47 +0500

    Nested Loops

---
+++ powerScripting/nested_loops2.ps1
@@ -0,0 +1,12 @@
+﻿for ($i = 1; $i -le 5; $i++){
+    $str=""
+    for ($k=$i+1; $k -le 5; $k++)
+    {
+        $str += " "
+    }
+
+    for ($j = 1;$j -le $i; $j++){
+        $str+= "* "
+    }
+    Write-Host $str
+}
