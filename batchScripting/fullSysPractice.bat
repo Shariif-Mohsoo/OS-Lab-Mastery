b5a20325a501e26859f9697ea9db76d2b38eed31
Shariif-Mohsoo <mr8781256@gmail.com>
2025-06-13 20:00:55 +0500

    Full Syllabus Covered

--- batchScripting/fullSysPractice.bat
+++ batchScripting/fullSysPractice.bat
@@ -1,4 +1,8 @@
 @echo off
-Title Full Syllabus Will Be Covered Here
-
+Title Full Syllabus Will Be Covered Here....
+REM Going to display the hello world
+echo Hello World
+REM How to use the variable
+set str=Hello Mohsoo
+echo %str%
 pause
